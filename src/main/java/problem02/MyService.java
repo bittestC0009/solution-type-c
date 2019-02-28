70224ec51f1839a293552bb55f717f794e4c957a
jusin <jusin1545@gmail.com>
2019-02-28 10:57:05 +0900

    b

--- src/main/java/problem02/MyService.java
+++ src/main/java/problem02/MyService.java
@@ -20,11 +20,16 @@ public class MyService extends BaseService {
 	public void service(String state) {
 		
 		if (state.equals("낮")) {
-			
+			String result = day();
+			System.out.println(result);
+		}else if (state.equals("오후")) {
+			String result = afternoon();
+			System.out.println(result);
+		}else if (state.equals("밤")) {
+			String result = night();
+			System.out.println(result);
 		}
 		
-		String result = state.equals("낮") ? day() : night() ;
-		System.out.println(result);
 		
 	}
 	
